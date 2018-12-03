f45b504637ba4c4c861888cd7b8c4de50ee391a9
vicarali <vicarali@gmail.com>
2018-12-03 21:39:59 +0100

    Segon commit del projecte mitjana

--- mitjana.c
+++ mitjana.c
@@ -12,7 +12,7 @@ int main() {
         suma=suma+nou_valor;
         numero_repeticions=numero_repeticions+1;
         printf("Vols continuar (s/n): ");
-        scanf(" %c",&consulta);
+        scanf("%s",&consulta);
     }
 
     mitjana=suma/numero_repeticions;
